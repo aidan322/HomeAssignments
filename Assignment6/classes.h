9d52b60fa4bcd318e3e6e98de65c0014701b9cbf
aidan322 <st133061@student.spbu.ru>
2025-04-17 17:02:40 +0300

    Add classes Class1, Class2, Class3with bar and test methods

---
+++ Assignment6/classes.h
@@ -0,0 +1,34 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+#include <vector>
+
+class Class1 {
+public:
+    bool bar(int x, std::vector<float>&) {
+        return x > 0;
+    }
+    int c_1_1() { return 42; }
+    float c_1_2() {return 3.14f; }
+    void c_1_3() {}
+};
+
+clas Class2 {
+public:
+    bool bar(int, std::vector<float>& v) {
+        return !v.empty();
+    }
+    int c_2_1() { return 7; }
+    float c_2_2() { return 2.71f; }
+    void c_2_3() {}
+};
+
+class Class3 {
+public:
+    bool bar(int x, std::vector<float>& v) {
+        return x == -1 * static_cast<int>(v.size());
+    int c_3_1() { return 9; }
+    float c_3_2() { return 1.23f; }
+    void c_3_3() {}
+};
+
+#endif // CLASSES_H
