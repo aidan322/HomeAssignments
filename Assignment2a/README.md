3c7c3904ff4994d54fdb77dc779585985ecfb89c
aidan322 <st133061@student.spbu.ru>
2025-04-21 17:15:54 +0300

    Final fix for Assignment2a: added file checks, updated README

--- Assignment2a/README.md
+++ Assignment2a/README.md
@@ -1,11 +1,18 @@
 # Assignment 2a
+
 ## Author
 Mongush Aidan Timurovich, 24.b82.mm
+
 ## Contacts
 st133061@student.spbu.ru
+
 ## Description
-Home assignment 2a - reading and writing binary files
+Assignment 2a - Program read binary file 'input.bin', reverses its contents, and writes to 'output.bin'.
+
 ## Build
 make
+
 ## Run
+echo -n "abcdef" > input.bin
 ./project2
+hexdump -C output.bin
--- Assignment2a/main.cpp
+++ Assignment2a/main.cpp
@@ -8,24 +8,31 @@ int main()
     std::string inputFile = "input.bin";   // the source file
     std::string outputFile = "output.bin"; // the source file
 
+    if(!std::filesystem::exists(inputFile)) {
+        std::cerr << "Error: file " << inputFile <<  " ' not found!" << std::endl;
+        return 1;
+    }
+
     size_t sizeFile = std::filesystem::file_size(inputFile);  //checking the file siza
 
-    std::ifstream inFile;  // openin the source file for reading
-    inFile.open(inputFile, std::ios::binary | std::ios::in);
+    std::ifstream inFile(inputFile, std::ios::binary);
+    if(!inFile) {
+        std::cerr << "Failed to open inputr file." << std::endl;
+    return 1;
+    }
 
     char* buffer = new char[sizeFile];   // allocating space for an array in the heap
-
-    inFile.read(buffer, sizeof(sizeFile));   // reading the contents of the file
+    inFile.read(buffer, sizeFile);   // reading the contents of the file
     inFile.close();
 
-    for(int i = 0; i < sizeFile/2; ++i) // rotating the contents of the file
+    for(size_t i = 0; i < sizeFile/2; ++i) // rotating the contents of the file
     {
         char tmp = buffer[i];
         buffer[i] = buffer[sizeFile - 1 - i];
         buffer[sizeFile - 1 -i] = tmp;
     }
 
-    std::ofstream outFile(outputFile, std::ios::binary | std::ios::out);  // opening the output file for writing
+    std::ofstream outFile(outputFile, std::ios::binary);  // opening the output file for writing
 
     outFile.write(buffer, sizeFile);
     outFile.close();
